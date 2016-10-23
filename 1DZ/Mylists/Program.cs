85efd1859254c37887d16848797005a053f577c6
TomislavHorvat <tom>
2016-10-23 21:33:43 +0200

    Napokon gotovo, radi i zvuk.

---
+++ 1DZ/Mylists/Program.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLists
+{
+    public interface IIntegerList
+    {
+        /// <summary>
+        /// Adds an item to the collection
+        /// </summary>
+        void Add(int item);
+
+        /// <summary>
+        /// Removes the first occurrence of an item from the collection.
+        /// If the item was not found, method does nothing.
+        /// </summary>
+        bool Remove(int index);
+
+        /// <summary>
+        /// Removes the item at the given index in the collection.
+        /// </summary>
+        bool RemoveAt(int index);
+
+        /// <summary>
+        /// Returns the item at the given index in the collection.
+        /// </summary>
+        int GetElement(int index);
+
+        /// <summary>
+        /// Returns the index of the item in the collection.
+        /// If item is nor found in the collection, method returns -1.
+        /// </summary>
+        int IndexOf(int item);
+
+        /// <summary>
+        /// Read only property. Gets the number of items contained in the collection.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Removes all items from the collection.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Determines whether the collections contains a specific value
+        /// </summary>
+        bool Contains(int item);
+    }
+
+
+    public interface IGenericList<X> : IEnumerable<X>
+    {
+        /// <summary>
+        /// Adds an item to the collection
+        /// </summary>
+        void Add(X item);
+
+        /// <summary>
+        /// Removes the first occurrence of an item from the collection.
+        /// If the item was not found, method does nothing.
+        /// </summary>
+        bool Remove(X index);
+
+        /// <summary>
+        /// Removes the item at the given index in the collection.
+        /// </summary>
+        bool RemoveAt(int index);
+
+        /// <summary>
+        /// Returns the item at the given index in the collection.
+        /// </summary>
+        X GetElement(int index);
+
+        /// <summary>
+        /// Returns the index of the item in the collection.
+        /// If item is nor found in the collection, method returns -1.
+        /// </summary>
+        int IndexOf(X item);
+
+        /// <summary>
+        /// Read only property. Gets the number of items contained in the collection.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Removes all items from the collection.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Determines whether the collections contains a specific value
+        /// </summary>
+        bool Contains(X item);
+    }
+
+    public class IntegerList : IIntegerList
+    {
+        private int[] _internalStorage;
+        private int _arraySize;
+        private int _count;
+
+        public IntegerList()
+        {
+            _internalStorage = new int[4];
+            _arraySize = 4;
+            _count = -1;
+        }
+
+        public IntegerList(int initialSize)
+        {
+            _internalStorage = new int[initialSize];
+            _arraySize = initialSize;
+            _count = -1;
+        }
+
+        public void Add(int item)
+        {
+            if (_count + 1 >= _arraySize)
+            {
+                int[] tmp = new int[_arraySize *= 2];
+                int index = 0;
+                foreach (int value in _internalStorage)
+                {
+                    tmp[index++] = value;
+                }
+                _internalStorage = tmp;
+            }
+            _internalStorage[++_count] = item;
+        }
+
+
+        public bool Remove(int item)
+        {
+            return RemoveAt(IndexOf(item));
+        }
+
+
+        public bool RemoveAt(int index)
+        {
+            if (index < 0 || index > _count)
+            {
+                return false;
+            }
+            while (index < _count)
+            {
+                _internalStorage[index] = _internalStorage[index + 1];
+                index++;
+            }
+            _count--;
+            return true;
+        }
+
+
+        public int GetElement(int index)
+        {
+            if (index >= 0 && index <= _arraySize)
+            {
+                return _internalStorage[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+
+        public int IndexOf(int item)
+        {
+            for (int i = 0; i <= _count; i++)
+            {
+                if (_internalStorage[i] == item)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count + 1;
+            }
+        }
+
+
+        public void Clear()
+        {
+            _internalStorage = null;
+            _count = -1;
+        }
+
+        public bool Contains(int item)
+        {
+            if (IndexOf(item) > -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+    public class GenericList<X> : IGenericList<X> where X : IComparable<X>
+    {
+        private X[] _internalStorage;       // bolje je koristiti ArrayList _innerArray;
+        private int _arraySize;
+        private int _count;
+
+        public GenericList()
+        {
+            _internalStorage = new X[4];
+            _arraySize = 4;
+            _count = -1;
+        }
+
+        public GenericList(int initialSize)
+        {
+            _internalStorage = new X[initialSize];
+            _arraySize = initialSize;
+            _count = -1;
+        }
+
+        public void Add(X item)
+        {
+            if (_count + 1 >= _arraySize)
+            {
+                X[] tmp = new X[_arraySize *= 2];
+                int index = 0;
+                foreach (X value in _internalStorage)
+                {
+                    tmp[index++] = value;
+                }
+                _internalStorage = tmp;
+            }
+            _internalStorage[++_count] = item;
+        }
+
+
+        public bool Remove(X item)
+        {
+            return RemoveAt(IndexOf(item));
+        }
+
+
+        public bool RemoveAt(int index)
+        {
+            if (index < 0 || index > _count)
+            {
+                return false;
+            }
+            while (index < _count)
+            {
+                _internalStorage[index] = _internalStorage[index + 1];
+                index++;
+            }
+            _count--;
+            return true;
+        }
+
+
+        public X GetElement(int index)
+        {
+            if (index >= 0 && index <= _arraySize)
+            {
+                return _internalStorage[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public int IndexOf(X item)
+        {
+            int index = 0;
+            foreach (X value in _internalStorage)
+            {
+                if (item.CompareTo(value) == 0)
+                {
+                    return index;
+                }
+                index++;
+            }
+            return -1;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count + 1;
+            }
+        }
+
+
+        public void Clear()
+        {
+            _internalStorage = null;
+            _count = -1;
+        }
+
+        public bool Contains(X item)
+        {
+            if (IndexOf(item) > -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerator<X> GetEnumerator()
+        {
+            return new GenericListEnumerator<X>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class GenericListEnumerator<X> : IEnumerator<X> where X : IComparable<X>
+    {
+        private GenericList<X> _collection = new GenericList<X>();
+        private int index;
+        private X _current;
+
+        public GenericListEnumerator()
+        {
+
+        }
+
+        public GenericListEnumerator(GenericList<X> collection)
+        {
+            _collection = collection;
+            index = -1;
+            _current = default(X);
+        }
+
+
+
+        public X Current
+        {
+            get
+            {
+                return _current;
+            }
+        }
+
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return _current;
+            }
+        }
+        public void Dispose()
+        {
+            _collection = null;
+            _current = default(X);
+            index = -1;
+        }
+
+        public bool MoveNext()
+        {
+            if (++index >= _collection.Count)
+            {
+                return false;
+            }
+            else
+            {
+                _current = _collection.GetElement(index);
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            _current = default(X);
+            index = -1;
+        }
+    }
+
+    public class Test
+    {
+        public static void ListExample(IIntegerList listOfIntegers)
+        {
+            listOfIntegers.Add(1);  // [1]
+            listOfIntegers.Add(2);  // [1, 2]
+            listOfIntegers.Add(3);  // [1, 2, 3]
+            listOfIntegers.Add(4);  // [1, 2, 3, 4]
+            listOfIntegers.Add(5);  // [1, 2, 3, 4, 5]
+
+            for (int i = 0; i < listOfIntegers.Count; i++)
+            {
+                Console.WriteLine(listOfIntegers.GetElement(i));
+            }
+
+            listOfIntegers.RemoveAt(0);
+            listOfIntegers.Remove(5);
+
+            Console.WriteLine();
+            for (int i = 0; i < listOfIntegers.Count; i++)
+            {
+                Console.WriteLine(listOfIntegers.GetElement(i));
+            }
+            Console.WriteLine();
+            Console.WriteLine(listOfIntegers.Count);
+            Console.WriteLine(listOfIntegers.Remove(100));
+            Console.WriteLine(listOfIntegers.RemoveAt(5));
+            listOfIntegers.Clear();
+            Console.WriteLine(listOfIntegers.Count);
+
+            IGenericList<string> stringList = new GenericList<string>();
+            stringList.Add("Hello");
+            stringList.Add("World");
+            stringList.Add("!");
+
+            Console.WriteLine();
+            foreach (string value in stringList)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+
+}
---
+++ 1DZ/Pong/Game1.cs
@@ -0,0 +1,361 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyLists;
+
+
+
+
+namespace Pong
+{
+    public interface IPhysicalObject2D
+    {
+        float X { get; set; }
+        float Y { get; set; }
+        int Width { get; set; }
+        int Height { get; set; }
+    }
+    public abstract class Sprite : IPhysicalObject2D , IComparable<Sprite>
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        /// <summary >
+        /// Represents the texture of the Sprite on the screen .
+        /// Texture2D is a type defined in Monogame framework .
+        /// </ summary >
+        public Texture2D Texture { get; set; }
+        protected Sprite(int width, int height, float x = 0, float y = 0)
+        {
+            X = x;
+            Y = y;
+            Height = height;
+            Width = width;
+        }
+        /// <summary >
+        /// Base draw method
+        /// </ summary >
+        public virtual void DrawSpriteOnScreen(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new Vector2(X, Y), new Rectangle(0, 0, Width, Height), Color.White);
+        }
+        public abstract int CompareTo(Sprite obj);
+    }
+    /// <summary >
+    /// Game background representation
+    /// </ summary >
+    public class Background : Sprite
+    {
+        public Background(int width, int height) : base(width, height)
+        {
+        }
+        public override int CompareTo(Sprite obj)
+        {
+            Background obj1 = (Background)obj;
+            return this.CompareTo(obj1);
+        }
+    }
+    /// <summary >
+    /// Game ball object representation
+    /// </ summary >
+    public class Ball : Sprite
+    {
+        /// <summary >
+        /// Defines current ball speed in time .
+        /// </ summary >
+        public float Speed { get; set; }
+        public float BumpSpeedIncreaseFactor { get; set; }
+        /// <summary >
+        /// Defines ball direction .
+        /// Valid values ( -1 , -1) , (1 ,1) , (1 , -1) , ( -1 ,1).
+        /// Using Vector2 to simplify game calculation . Potentially
+        /// dangerous because vector 2 can swallow other values as well .
+        /// OPTIONAL TODO : create your own , more suitable type
+        /// </ summary >
+        public Vector2 Direction { get; set; }
+        public Ball(int size, float speed, float
+        defaultBallBumpSpeedIncreaseFactor) : base(size, size)
+        {
+            Speed = speed;
+            BumpSpeedIncreaseFactor = defaultBallBumpSpeedIncreaseFactor;
+            // Initial direction
+            Direction = new Vector2(1, 1);
+        }
+        public override int CompareTo(Sprite obj)
+        {
+            Background obj1 = (Background)obj;
+            return this.CompareTo(obj1);
+        }
+    }
+    /// <summary >
+    /// Represents player paddle .
+    /// </ summary >
+    public class Paddle : Sprite
+    {
+        /// <summary >
+        /// Current paddle speed in time
+        /// </ summary >
+        public float Speed { get; set; }
+        public Paddle(int width, int height, float initialSpeed) : base(width,height)
+        {
+            Speed = initialSpeed;
+        }
+        /// <summary >
+        /// Overriding draw method . Masking paddle texture with black color .
+        /// </ summary >
+        public override void DrawSpriteOnScreen(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new Vector2(X, Y), new Rectangle(0, 0, Width, Height), Color.GhostWhite);
+        }
+        public override int CompareTo(Sprite obj)
+        {
+            Paddle obj1 = (Paddle)obj;
+            return this.CompareTo(obj1);
+        }
+    }
+    public class GameConstants
+    {
+        public const float PaddleDefaulSpeed = 0.9f ;
+        public const int PaddleDefaultWidth = 200;
+        public const int PaddleDefaulHeight = 20;
+        public const float DefaultInitialBallSpeed = 0.4f ;
+        public const float DefaultBallBumpSpeedIncreaseFactor = 1.05f ;
+        public const int DefaultBallSize = 40;
+        public const int WallDefaultSize = 0;
+    }
+    public class CollisionDetector
+    {
+        public static bool Overlaps(IPhysicalObject2D a, IPhysicalObject2D b)
+        {
+            if (a.X <= b.X + b.Width &&
+                a.X + a.Width >= b.X &&
+                a.Y <= b.Y + b.Height &&
+                a.Y + a.Height >= b.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        public class Wall : IPhysicalObject2D
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public Wall(float x, float y, int width, int height)
+            {
+                X = x;
+                Y = y;
+                Width = width;
+                Height = height;
+            }
+        }
+        public List<Wall> Walls { get; set; }
+        public List<Wall> Goals { get; set; }
+
+        /// <summary >
+        /// Bottom paddle object
+        /// </ summary >
+        public Paddle PaddleBottom { get; private set; }
+        /// <summary >
+        /// Top paddle object
+        /// </ summary >
+        public Paddle PaddleTop { get; private set; }
+        /// <summary >
+        /// Ball object
+        /// </ summary >
+        public Ball Ball { get; private set; }
+        /// <summary >
+        /// Background image
+        /// </ summary >
+        public Background Background { get; private set; }
+        /// <summary >
+        /// Sound when ball hits an obstacle .
+        /// SoundEffect is a type defined in Monogame framework
+        /// </ summary >
+        public SoundEffect HitSound { get; private set; }
+        /// <summary >
+        /// Background music . Song is a type defined in Monogame framework
+        /// </ summary >
+        public Song Music { get; private set; }
+        /// <summary >
+        /// Generic list that holds Sprites that should be drawn on screen
+        /// </ summary >
+        private IGenericList<Sprite> SpritesForDrawList = new GenericList<Sprite>();
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferHeight = 900,
+                PreferredBackBufferWidth = 500
+            };
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            // Screen bounds details . Use this information to set up game objects
+            //positions.
+            var screenBounds = GraphicsDevice.Viewport.Bounds;
+            PaddleBottom = new Paddle(GameConstants.PaddleDefaultWidth,
+            GameConstants.PaddleDefaulHeight, GameConstants.PaddleDefaulSpeed);
+            PaddleBottom.X = 500 / 2 - GameConstants.PaddleDefaultWidth / 2;
+            PaddleBottom.Y = 900 - GameConstants.PaddleDefaulHeight;
+            PaddleTop = new Paddle(GameConstants.PaddleDefaultWidth,
+            GameConstants.PaddleDefaulHeight, GameConstants.PaddleDefaulSpeed);
+            PaddleTop.X = 500 / 2 - GameConstants.PaddleDefaultWidth / 2;
+            PaddleTop.Y = 0;
+            Ball = new Ball(GameConstants.DefaultBallSize,
+            GameConstants.DefaultInitialBallSpeed,
+            GameConstants.DefaultBallBumpSpeedIncreaseFactor);
+            {
+                Ball.X = 500 / 2 - GameConstants.DefaultBallSize / 2;
+            Ball.Y = 900 / 2 - GameConstants.DefaultBallSize / 2;
+            }
+        Background = new Background(500 , 900);
+        // Add our game objects to the sprites that should be drawn collection ..
+        // you ’ll see why in a second
+            SpritesForDrawList.Add(Background);
+            SpritesForDrawList.Add(PaddleBottom);
+            SpritesForDrawList.Add(PaddleTop);
+            SpritesForDrawList.Add(Ball);
+
+            Walls = new List<Wall>()
+            {
+                // try with 100 for default wall size !
+                new Wall ( - GameConstants.WallDefaultSize ,0 ,
+                GameConstants.WallDefaultSize , screenBounds . Height ) ,
+                new Wall ( screenBounds . Right ,0 , GameConstants . WallDefaultSize ,
+                screenBounds . Height ) ,
+            };
+            Goals = new List<Wall>()
+            {
+                new Wall (0 , screenBounds . Height , screenBounds . Width ,
+                GameConstants.WallDefaultSize ) ,
+                new Wall ( screenBounds . Top , - GameConstants . WallDefaultSize ,
+                screenBounds . Width , GameConstants . WallDefaultSize ) ,
+            };
+            base.Initialize();
+
+        }
+        protected override void LoadContent()
+        {
+            // Initialize new SpriteBatch object which will be used to draw textures .
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Set textures
+            Texture2D paddleTexture = Content.Load<Texture2D>("paddle");
+            PaddleBottom.Texture = paddleTexture;
+            PaddleTop.Texture = paddleTexture;
+            Ball.Texture = Content.Load<Texture2D>("ball");
+            Background.Texture = Content.Load<Texture2D>("background");
+            // Load sounds
+            // Start background music
+            HitSound = Content.Load<SoundEffect>("hit");
+            Music = Content.Load<Song>("music");
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(Music);
+        }
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            var screenBounds = GraphicsDevice.Viewport.Bounds;
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            var touchState = Keyboard.GetState();
+            if (touchState.IsKeyDown(Keys.Left))
+            {
+                PaddleBottom.X = PaddleBottom.X - (float)(PaddleBottom.Speed * gameTime.ElapsedGameTime.TotalMilliseconds);
+                PaddleBottom.X = MathHelper.Clamp(PaddleBottom.X, screenBounds.Left, screenBounds.Right - PaddleBottom.Width);
+            }
+            if (touchState.IsKeyDown(Keys.Right))
+            {
+                PaddleBottom.X = PaddleBottom.X + (float)(PaddleBottom.Speed * gameTime.ElapsedGameTime.TotalMilliseconds);
+                PaddleBottom.X = MathHelper.Clamp(PaddleBottom.X, screenBounds.Left, screenBounds.Right - PaddleBottom.Width);
+            }
+            if (touchState.IsKeyDown(Keys.A))
+            {
+                PaddleTop.X = PaddleTop.X - (float)(PaddleTop.Speed * gameTime.ElapsedGameTime.TotalMilliseconds);
+                PaddleTop.X = MathHelper.Clamp(PaddleTop.X, screenBounds.Left, screenBounds.Right - PaddleTop.Width);
+            }
+            if (touchState.IsKeyDown(Keys.D))
+            {
+                PaddleTop.X = PaddleTop.X + (float)(PaddleTop.Speed * gameTime.ElapsedGameTime.TotalMilliseconds);
+                PaddleTop.X = MathHelper.Clamp(PaddleTop.X, screenBounds.Left, screenBounds.Right - PaddleTop.Width);
+
+            }
+            var ballPositionChange = Ball.Direction * (float)(gameTime.ElapsedGameTime.TotalMilliseconds * Ball.Speed);
+            Ball.X += ballPositionChange.X;
+            Ball.Y += ballPositionChange.Y;
+
+            // Ball - side walls
+            if (Walls.Any(w => CollisionDetector.Overlaps(Ball, w)))
+            {
+                Ball.Direction = Ball.Direction * new Vector2(-1, 1);
+                Ball.Speed = Ball.Speed * Ball.BumpSpeedIncreaseFactor;
+            }
+            // Ball - winning walls
+            if (Goals.Any(w => CollisionDetector.Overlaps(Ball, w)))
+            {
+                Ball.X = screenBounds.Center.ToVector2().X;
+                Ball.Y = screenBounds.Center.ToVector2().Y;
+                Ball.Speed = GameConstants.DefaultInitialBallSpeed;
+                HitSound.Play();
+            }
+            // Paddle - ball collision
+            if (CollisionDetector.Overlaps(Ball, PaddleTop) && Ball.Direction.Y < 0
+                || (CollisionDetector.Overlaps(Ball, PaddleBottom) && Ball.Direction.Y > 0))
+            {
+                Ball.Direction = Ball.Direction * new Vector2(1, -1);
+                Ball.Speed *= Ball.BumpSpeedIncreaseFactor;
+            }
+
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            // Start drawing.
+            spriteBatch.Begin();
+            for (int i = 0; i < SpritesForDrawList.Count; i++)
+            {
+                SpritesForDrawList.GetElement(i).DrawSpriteOnScreen(spriteBatch);
+            }
+            // End drawing.
+            // Send all gathered details to the graphic card in one batch .
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
